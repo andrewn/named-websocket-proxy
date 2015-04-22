95d1f5ad72769a552d39b3ad5980a51326214162
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-04-22 21:45:08 +0100

    External proxy for managing connections between proxies

--- background/src/app.js
+++ background/src/app.js
@@ -1,23 +1,40 @@
-var debug = require('./debug')('App'),
+var debug = require('./debug'),
+    appLogger = debug('App'),
     ProxyServer = require('./proxy-server'),
+    PeerDiscovery = require('./peer-discovery'),
     Channel = require('./channel'),
-    Peer = require('./peer');
+    Peer = require('./peer'),
+    _ = require('lodash');
 
 var App = function () {
   this.initialized = false;
+
+
 };
 
 App.prototype.init = function () {
-  debug.info('init');
+  appLogger.info('init');
 
   if (this.initialized) {
-    debug.warn('Already initialized');
+    appLogger.warn('Already initialized');
     return;
   }
 
+  chrome.system.network.getNetworkInterfaces(function (interfaces) {
+    console.log('interfaces', interfaces);
+    var ip = _.find(interfaces, { prefixLength: 24 });
+    if (!ip) { throw Error('cannot find IP address'); }
+
+    this.startWithPublicIp(ip.address);
+  }.bind(this));
+}
+
+App.prototype.startWithPublicIp = function (publicIp) {
+  appLogger.info('starting with public IP', publicIp);
+
   this.initialized = true;
 
-  this.publicIp = '192.168.0.7';
+  this.publicIp   = publicIp;
   this.publicPort = 0;
 
   this.localIp   = '127.0.0.1';
@@ -42,6 +59,17 @@ App.prototype.init = function () {
   // WebSocket connections to other proxies
   this.proxies = [];
 
+  this.createLocalProxy();
+  this.createExternalProxy();
+};
+
+App.prototype.createLocalProxy = function () {
+  /*
+    Proxy server listens on well-known port 9009.
+    Handles incoming websockets from the same device and routes
+    them to where they needs to go.
+  */
+  var proxyLogger = debug('LocalProxy');
   this.proxyServer = new ProxyServer(this.localIp, this.localPort);
   this.proxyServer.on('connection', function (channelName, socket) {
     var channel = Channel.find(channelName, this.channels),
@@ -54,17 +82,17 @@ App.prototype.init = function () {
 
     peer = { id: Peer.id(), socket: socket, channel: channelName };
 
-    debug.log('New local peer', peer);
+    proxyLogger.log('New local peer', peer);
 
-    debug.log('before localPeers', this.localPeers.length);
+    proxyLogger.log('before localPeers', this.localPeers.length);
 
     Channel.connectPeers(peer, this.localPeers);
     this.localPeers.push(peer);
 
-    debug.log('after localPeers', this.localPeers.length);
+    proxyLogger.log('after localPeers', this.localPeers.length);
 
     socket.addEventListener('message', function (evt) {
-      debug.log('socket.message: ', evt);
+      proxyLogger.log('socket.message: ', evt);
 
       var peers = Channel.peers(channel, this.localPeers);
 
@@ -76,26 +104,27 @@ App.prototype.init = function () {
       }
 
       if (payload.action === 'broadcast') {
-        debug.log('Broadcast action: ', payload);
+        proxyLogger.log('Broadcast action: ', payload);
         Channel.broadcastMessage(peer, peers, payload.data);
       }
       else if (payload.action === 'message') {
-        debug.log('Direct message action: ', payload);
+        proxyLogger.log('Direct message action: ', payload);
         target = Peer.find(payload.target, this.localPeers);
         if (target) {
-          debug.log('Sending to local peer: ', payload);
+          proxyLogger.log('Sending to local peer: ', payload);
           Channel.directMessage(peer, target, payload.data);
           return;
         }
 
         target = Peer.find(payload.target, this.remotePeers);
         if (target) {
-          debug.log('Sending to remote peer: ', payload);
+          proxyLogger.log('Sending to remote peer: ', payload);
+          // TODO:
           // Channel.directMessage(channel, payload.data, this.localPeers);
           return;
         }
 
-        debug.warn('Message for peer that cannot be found: ', payload);
+        proxyLogger.warn('Message for peer that cannot be found: ', payload);
 
         targetPeer = channel.getPeerById(payload.target);
         if (targetPeer) {
@@ -106,26 +135,65 @@ App.prototype.init = function () {
     }.bind(this));
 
     socket.addEventListener('close', function (evt) {
-      debug.log('socket.close: before remove', peer, this.localPeers.length, this.localPeers);
+      proxyLogger.log('socket.close: before remove', peer, this.localPeers.length, this.localPeers);
 
       // Remove this peer
       Peer.remove(peer, this.localPeers);
 
-      debug.log('after remove', peer, this.localPeers.length, this.localPeers);
+      proxyLogger.log('after remove', peer, this.localPeers.length, this.localPeers);
 
       // Disconnect localPeers in channel
       Channel.disconnectPeers(peer, Channel.peers(channel, this.localPeers));
 
-      debug.log('Removed local peer', peer);
+      proxyLogger.log('Removed local peer', peer);
+    }.bind(this));
+  }.bind(this));
+}
+
+App.prototype.createExternalProxy = function () {
+  /*
+    External Proxy server listens on any available port and on
+    the external interface.
+    Handles incoming websockets from the other proxies and removes
+    disconnections
+  */
+  var externalLogger = debug('ExternalProxy');
+  this.externalServer = new ProxyServer(this.publicIp, this.publicPort);
+  this.externalServer.on('connection', function (url, socket, request) {
+
+    externalLogger.log('New external request', socket, url, request);
+
+    var ip = request.headers.Host,
+        proxy = _.find(this.proxies, { ip: ip, socket: socket });
+
+    if (proxy) {
+      throw Error('Proxy from ip ' + proxy.ip + ' already exists');
+      return;
+    }
+
+    externalLogger.log('New connection from ip:', ip);
+    proxy = { ip: ip, socket: socket };
+    this.proxies.push(proxy);
+
+    socket.addEventListener('message', function (evt) {
+      externalLogger.log('socket.message: ', evt);
+      // TODO: route message to localPeers on this message
     }.bind(this));
 
+    socket.addEventListener('close', function (evt) {
+      externalLogger.log('socket.close, removing proxy connection');
+      _.remove(this.proxies, { ip: ip });
+      // TODO: Remove remote peers using this connection?
+      // TODO: Notify local peers
+
+    }.bind(this));
 
   }.bind(this));
 
 };
 
 App.prototype.destroy = function () {
-  debug.info('destroy');
+  appLogger.info('destroy');
 };
 
 module.exports = App;
---
+++ background/src/proxy-server.js
@@ -0,0 +1,56 @@
+var debug = require('./debug')('ProxyServer'),
+    HttpServer = require('../lib/http-server'),
+    WebSocketServer = require('../lib/websocket-server'),
+    EventEmitter = require('events').EventEmitter,
+    inherits = require('util').inherits;
+
+var ProxyConnections = require('./proxy-connections'),
+    Peer = require('./peer'),
+    protocol = require('./shim-protocol');
+
+var Proxy = function (address, port) {
+
+  this.httpServer_ = new HttpServer(debug);
+  this.wsServer_ = new WebSocketServer(this.httpServer_);
+  this.httpServer_.listen(port, address);
+
+  this.httpServer_.addEventListener('request', function (req) {
+    debug.log('An HTTP request!', req);
+    req.writeHead(404, {});
+    req.end();
+
+    // Keep socket open until we explicitly
+    // call req.end()
+    return true;
+  });
+
+  this.wsServer_.addEventListener('request', function (req) {
+    debug.log('WebSocket request', req);
+    var keepConnectionOpen, channelName, socket;
+
+    if ('url' in req.headers) {
+      channelName = req.headers.url.replace(/^\//, '');
+    }
+
+    if (!channelName) {
+      req.reject();
+      keepConnectionOpen = false;
+      return keepConnectionOpen;
+    } else {
+      socket = req.accept();
+      keepConnectionOpen = true;
+    }
+
+    this.emit('connection', channelName, socket, req);
+
+    return keepConnectionOpen;
+  }.bind(this));
+};
+
+inherits(Proxy, EventEmitter);
+
+Proxy.prototype.disconnect = function () {
+  this.httpServer_.disconnect();
+}
+
+module.exports = Proxy;
