bc1df7bfa4b514cbc856991b1d2c805bb019821b
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-04-22 23:17:13 +0100

    Add warn to logger

--- background/src/debug.js
+++ background/src/debug.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 
 module.exports = function (thing) {
   var instance = {};
-  ['log', 'info', 'error', 'log'].forEach(function (type) {
+  ['log', 'info', 'warn', 'error', 'log'].forEach(function (type) {
     instance[type] = function () {
       console[type].apply(console, [thing + ': '].concat( _.toArray(arguments) ));
     };
