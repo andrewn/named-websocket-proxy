86a71a32a392bb43dbb85e276861409f8b9fe5f5
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-04-22 18:20:39 +0100

    Pass messages down shared socket

--- background/src/proxy-connection.js
+++ background/src/proxy-connection.js
@@ -1,4 +1,38 @@
-var AmpersandState = require('ampersand-state');
+var AmpersandState = require('ampersand-state'),
+    EventSource = require('../lib/event-source');
+
+var ShimSocket = function (sharedSocket, peerId) {
+  var self = this;
+  this.sharedSocket = sharedSocket;
+  this.peerId = peerId;
+  this.messageQueue = [];
+
+  this.sharedSocket.addEventListener('message', function (msg) {
+    console.log('ShimSocket.message:', msg);
+  });
+
+  this.sharedSocket.addEventListener('open', function () {
+    console.log('ProxyConnection - sharedSocket open, sending queued messages');
+    self.messageQueue.forEach(function (msg) {
+      self.send(msg);
+    });
+  });
+
+  this.sharedSocket.addEventListener('close', function () {
+    console.warn('ProxyConnection - sharedSocket close!');
+  });
+};
+
+ShimSocket.prototype = new EventSource();
+
+ShimSocket.prototype.send = function (msg) {
+  if (this.sharedSocket.readyState !== 1) {
+    console.log('ProxyConnection - shimSocket.send connection not ready, queueing');
+    this.messageQueue.push(msg);
+  }
+  console.log('ProxyConnection - shimSocket.send', this.peerId, msg);
+  this.sharedSocket.send(msg);
+};
 
 /*
   Represents a WebSocket connection
@@ -11,25 +45,16 @@ var ProxyConnection = AmpersandState.extend({
   },
   initialize: function () {
     console.log('ProxyConnection.initialize', this.socket);
-    this.socket.addEventListener('message', function () {
-      console.log('ProxyConnection.message', this.ip);
-    }.bind(this));
   }
 });
 
 /*
   Returns a shim socket connection that emits events
-  only for this peer
+  only for this peer and sends messages down the socket
+  to that peer's proxy
 */
 ProxyConnection.prototype.socketForPeer = function (id) {
-  return {
-    send: function (msg) {
-      console.log('ProxyConnection - shimSocket.send', id, msg);
-    },
-    addEventListener: function (eventName) {
-      console.log('ProxyConnection - shimSocket.listen', eventName);
-    }
-  };
+  return new ShimSocket(this.socket, id);
 }
 
 module.exports = ProxyConnection;
