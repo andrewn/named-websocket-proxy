e1803894ea47f3d1b0b3d24053263780f5f6c6ba
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-04-28 15:52:18 +0100

    Allow logging level to be set

--- background/src/debug.js
+++ background/src/debug.js
@@ -1,11 +1,23 @@
 var _ = require('lodash');
 
+var logLevels = ['log', 'info', 'warn', 'error', 'log'],
+    logLevel = 'log',
+    currentLevelIndex = logLevels.indexOf(logLevel);
+
 module.exports = function (thing) {
   var instance = {};
-  ['log', 'info', 'warn', 'error', 'log'].forEach(function (type) {
+  logLevels.forEach(function (type) {
     instance[type] = function () {
-      console[type].apply(console, [thing + ': '].concat( _.toArray(arguments) ));
+      var indexOfLevel = logLevels.indexOf(type);
+      if (indexOfLevel >= currentLevelIndex) {
+        console[type].apply(console, [thing + ': '].concat( _.toArray(arguments) ));
+      }
     };
   });
   return instance;
+};
+
+module.exports.setLogLevel = function (level) {
+  logLevel = level;
+  currentLevelIndex = logLevels.indexOf(logLevel);
 };
